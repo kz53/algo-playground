2b63a3a9a6dc884ad355fd20ad5a8dcfb1d1e454
Arthur Chen <ac2266@cornell.edu>
2020-07-01 18:42:39 -0700

    Switched to using __call__

--- nn_model.py
+++ nn_model.py
@@ -1,7 +1,7 @@
 import talib as ta
 import numpy as np
 import tensorflow as tf
-
+import time
 
 def init():
     global model1
@@ -11,7 +11,9 @@ def init():
 
 
 def get_pred(high,low,close):
+    #NOTE TAKES 15 minutes worth of highs, lows and closes.
     new=np.zeros(10)
+    t0=time.clock()
     new[0]=ta.PLUS_DM(high,low)[-1]
     new[1]=ta.CCI(high,low,close)[-1]
     new[2]=ta.CMO(close)[-1]
@@ -22,9 +24,15 @@ def get_pred(high,low,close):
     new[7]=ta.RSI(close)[-1]
     new[8]=ta.WILLR(high,low,close)[-1]
     new[9]=ta.ROC(close,timeperiod=14)[-1]*100
+    t1=time.clock()
+    #print("Formating time {}".format(t1-t0))
     new=(new/100).reshape((1,10))
-    val1=model1.predict(new)
-    #=model2.predict(new)
+    t0=time.clock()
+    val1=model1(new)
+    t1=time.clock()
+
+    #print("Predict Time: {}".format(t1-t0))
+    #val2=model2.predict(new)
     #print(val1,val2)
 
-    return (val1>.5)
+    return (val1>.5)# and (val2>.5)
